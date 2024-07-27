38873cc1b200a2d88268cd0c267799228bc39d4e
jiuhao <815514981@qq.com>
2024-07-27 08:03:37 +0800

    update

--- font.lua
+++ font.lua
@@ -1,29 +1,29 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local font_size = 15
-local bold = true
+local font_size = 17
+local bold = false
 local font_family = ({
-       'Iosevka Nerd Font Mono', -- [1]
-       'JetBrainsMono Nerd Font', -- [2]
-       'FiraCode Nerd Font Mono', -- [3]
-       'Photonico Code', -- [4]
-       'VictorMono Nerd Font', -- [5]
-       'Hurmit Nerd Font Mono', -- [6]
-       'Gintronic', -- [7]
-       'Monocraft', -- [8]
-       'Comic Mono', -- [9]
-       'Input Mono', -- [10]
-       'ComicCode Nerd Font Mono', -- [11]
-    })[3]
+	"Iosevka Nerd Font Mono", -- [1]
+	"JetBrainsMono Nerd Font", -- [2]
+	"FiraCode Nerd Font Mono", -- [3]
+	"Photonico Code", -- [4]
+	"VictorMono Nerd Font", -- [5]
+	"Hurmit Nerd Font Mono", -- [6]
+	"Gintronic", -- [7]
+	"Monocraft", -- [8]
+	"Comic Mono", -- [9]
+	"Input Mono", -- [10]
+	"ComicCode Nerd Font Mono", -- [11]
+})[3]
 
 local options = {}
 if bold then
-   options['weight'] = 'Bold'
+	options["weight"] = "Bold"
 end
 
 local font = wezterm.font(font_family, options)
 
 return {
-   font      = font,
-   font_size = font_size,
+	font = font,
+	font_size = font_size,
 }
