e5bd2e67c4ef441e75ccca0f77588eb9ae94d7ed
jiuhao <815514981@qq.com>
2024-10-21 10:51:32 +0800

    update

--- window.lua
+++ window.lua
@@ -14,7 +14,6 @@ local M = {
 	},
 	adjust_window_size_when_changing_font_size = false,
 	-- How many lines of scrollback you want to retain per tab
-	--scrollback_lines = 130000000,
 	scrollback_lines = 300,
 }
 
